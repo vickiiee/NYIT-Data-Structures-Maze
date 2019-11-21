4dcf4429327ed401fa16566dc5c15028b44df906
DFTA <DFTA@216.37.97.98>
2019-11-21 16:23:16 -0500

    Got the basics down but got minor things to change for this code to get the output

--- Maze/src/Location.java
+++ Maze/src/Location.java
@@ -1,8 +1,78 @@
-
 public class Location {
-
-	public Location() {
-		// TODO Auto-generated constructor stub
+		int rowLocation;
+		int colLocation;
+		void setLocation(int row, int col){
+			this.rowLocation=row;
+			this.colLocation = col;
+		}
+		
+		int getRowLocation(){
+			return rowLocation;
+		}
+		
+		int getColLocation(){
+			return colLocation;
+		}
 	}
 
-}
+
+	class Node<T>{
+		T data;
+	    Node next;
+	    public Node(T data){
+	        this.data=data;
+	    }
+	    public void displayNode(){
+	        System.out.print(data+ " ");
+	    }
+	}
+
+
+	class LinkList <T> {
+
+	    
+	    private Node first = null;
+	    private Node last = null;
+
+	    public boolean isEmpty() {
+	        return (first == null);
+	    }
+
+	    public <T> void addLast(T data) {
+	        Node n = new Node(data);
+	        if (isEmpty()) {
+	            n.next = null;
+	            first = n;
+	            last = n;
+	        } else {
+	            last.next = n;
+	            last = n;
+	            last.next = null;
+	        }
+
+	    }
+
+	    public T removeFirst() {
+
+	            Node temp = first;
+	            if(first != null){
+	            	if (first.next == null)
+	            		last = null;
+	            	first = first.next;
+	            	temp.next=null;
+	            	return (T) temp.data;
+	            }else{
+	            	return null;
+	            }
+	        }
+
+
+	    public void displayList() {
+	        Node current = first;
+	        while (current != null) {
+	            current.displayNode();
+	            current = current.next;
+	        }
+	    }
+
+	}
