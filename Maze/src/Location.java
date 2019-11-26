c01bf3efc6f35e03aac2df9995c3663898bf01f4
DFTA <DFTA@216.37.96.175>
2019-11-26 16:16:09 -0500

    Changed the file

--- Maze/src/Location.java
+++ Maze/src/Location.java
@@ -1,78 +1,16 @@
 public class Location {
-		int rowLocation;
-		int colLocation;
-		void setLocation(int row, int col){
-			this.rowLocation=row;
-			this.colLocation = col;
-		}
-		
-		int getRowLocation(){
-			return rowLocation;
-		}
-		
-		int getColLocation(){
-			return colLocation;
-		}
+	int rowLocation;
+	int colLocation;
+	void setLocation(int row, int col){
+		this.rowLocation=row;
+		this.colLocation = col;
 	}
-
-
-	class Node<T>{
-		T data;
-	    Node next;
-	    public Node(T data){
-	        this.data=data;
-	    }
-	    public void displayNode(){
-	        System.out.print(data+ " ");
-	    }
+	
+	int getRowLocation(){
+		return rowLocation;
 	}
-
-
-	class LinkList <T> {
-
-	    
-	    private Node first = null;
-	    private Node last = null;
-
-	    public boolean isEmpty() {
-	        return (first == null);
-	    }
-
-	    public <T> void addLast(T data) {
-	        Node n = new Node(data);
-	        if (isEmpty()) {
-	            n.next = null;
-	            first = n;
-	            last = n;
-	        } else {
-	            last.next = n;
-	            last = n;
-	            last.next = null;
-	        }
-
-	    }
-
-	    public T removeFirst() {
-
-	            Node temp = first;
-	            if(first != null){
-	            	if (first.next == null)
-	            		last = null;
-	            	first = first.next;
-	            	temp.next=null;
-	            	return (T) temp.data;
-	            }else{
-	            	return null;
-	            }
-	        }
-
-
-	    public void displayList() {
-	        Node current = first;
-	        while (current != null) {
-	            current.displayNode();
-	            current = current.next;
-	        }
-	    }
-
+	
+	int getColLocation(){
+		return colLocation;
 	}
+}
--- Maze/src/LocationQueue.java
+++ Maze/src/LocationQueue.java
@@ -4,5 +4,24 @@ public class LocationQueue {
 	public LocationQueue() {
 		// TODO Auto-generated constructor stub
 	}
+	 public  void enqueue(Location data) {
+		 	locationList.addLast(data);
+	    }
 
+	    public Location dequeue() {
+	        if(!locationList.isEmpty())
+	        	return locationList.removeFirst();
+	        else
+	        	return null;
+
+	    }
+
+	    public void displayQueue() {
+	    	locationList.displayList();
+	        System.out.println();
+	    }
+
+	    public boolean isEmpty(){
+	        return locationList.isEmpty();
+	    }
 }
